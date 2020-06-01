04063433cfa25680a85c626bad101cd21c45a4b7
Juan Monterrosa Sanchez <38740763+jcmonterrosas@users.noreply.github.com>
2020-06-01 16:41:11 -0500

    🚧 Capture token

---
+++ app/src/main/java/com/example/pinart_ma/service/MyFirebaseMessagingService.kt
@@ -0,0 +1,34 @@
+package com.example.pinart_ma.service
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService(){
+
+    val TAG = "FCM Service"
+
+    /*
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.from)
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.notification != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body)
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }*/
+
+    override fun onNewToken(p0: String?) {
+        super.onNewToken(p0)
+
+        Log.d(TAG, "The Token refreshed $p0")
+
+        sendRegistrationToServer(p0)
+    }
+
+}
